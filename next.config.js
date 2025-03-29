a0c9bb88ccfbb38844f5606b539a22530b5dfff2
gasimmons <gasimmons@ucsd.edu>
2025-03-29 16:28:13 -0700

    Fixed Vercel Deployment

--- next.config.js
+++ next.config.js
@@ -1,23 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    reactStrictMode: true,
-    swcMinify: true,
-    basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
-
     images: {
         unoptimized: true,
-        domains: ["placeholder.svg"],
-        remotePatterns: [{
-            protocol: "https",
-            hostname: "**",
-        }],
     },
-
-    experimental: {
-        appDir: true,
-    }
 }
 
 module.exports = nextConfig
